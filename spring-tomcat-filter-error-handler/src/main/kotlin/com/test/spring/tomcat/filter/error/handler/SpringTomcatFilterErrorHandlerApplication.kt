1c9a88a70a8fe847855fe0400ae8ac4c849659ed
JonasGao <jonas.gao@voyageone.cn>
2017-11-22 14:52:45 +0800

    add exception handlers

--- spring-tomcat-filter-error-handler/src/main/kotlin/com/test/spring/tomcat/filter/error/handler/SpringTomcatFilterErrorHandlerApplication.kt
+++ spring-tomcat-filter-error-handler/src/main/kotlin/com/test/spring/tomcat/filter/error/handler/SpringTomcatFilterErrorHandlerApplication.kt
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver
 import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @SpringBootApplication
 class SpringTomcatFilterErrorHandlerApplication : SpringBootServletInitializer() {
@@ -20,6 +27,7 @@ fun main(args: Array<String>) {
 }
 
 @Component
+@Order(0)
 class MyFilter : Filter {
     override fun destroy() {
     }
@@ -34,4 +42,31 @@ class MyFilter : Filter {
 
 @Controller class MyController {
     @GetMapping("/") fun index() = "index"
+}
+
+@Component
+@ControllerAdvice
+class ExceptionHandler : AbstractHandlerExceptionResolver() {
+
+    override fun doResolveException(request: HttpServletRequest?, response: HttpServletResponse, handler: Any?, ex: java.lang.Exception?): ModelAndView? {
+        if (!response.isCommitted) {
+            response.reset()
+        }
+
+        response.writer.use {
+            println("Hello, Exception!")
+        }
+
+        return null
+    }
+
+    @ExceptionHandler(Exception::class) fun handler(response: HttpServletResponse) {
+        if (!response.isCommitted) {
+            response.reset()
+        }
+
+        response.writer.use {
+            println("Hello, Exception!")
+        }
+    }
 }
