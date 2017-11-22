3e9687101e826180d110f922760096bb7bdaf9fe
JonasGao <jonas.gao@voyageone.cn>
2017-11-22 14:11:14 +0800

    add tomcat deploy support

--- spring-tomcat-filter-error-handler/src/main/kotlin/com/test/spring/tomcat/filter/error/handler/SpringTomcatFilterErrorHandlerApplication.kt
+++ spring-tomcat-filter-error-handler/src/main/kotlin/com/test/spring/tomcat/filter/error/handler/SpringTomcatFilterErrorHandlerApplication.kt
@@ -2,10 +2,36 @@ package com.test.spring.tomcat.filter.error.handler
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import javax.servlet.*
 
 @SpringBootApplication
-class SpringTomcatFilterErrorHandlerApplication
+class SpringTomcatFilterErrorHandlerApplication : SpringBootServletInitializer() {
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder =
+            builder.sources(SpringTomcatFilterErrorHandlerApplication::class.java)
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(SpringTomcatFilterErrorHandlerApplication::class.java, *args)
+}
+
+@Component
+class MyFilter : Filter {
+    override fun destroy() {
+    }
+
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
+        throw IllegalStateException("This is a test exception")
+    }
+
+    override fun init(filterConfig: FilterConfig?) {
+    }
+}
+
+@Controller class MyController {
+    @GetMapping("/") fun index() = "index"
 }
